6ef20559db2bdf00a0f67b4894e95d2ec376c3b6
Nicolas Selig <triptonn.dev@gmail.com>
2025-08-11 12:37:34 +0200

    v0.1.36: Split of from game_of_loife repo

---
+++ core/interfaces/Inert.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import data.Vec;
+
+public interface Inert {
+    Vec getLocation();
+
+    void setLocation(Vec loc);
+
+    double getAngle();
+
+    void setAngle(double angle);
+}
